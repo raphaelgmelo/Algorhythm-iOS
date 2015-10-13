174f73c64a2fb0606cf367c6651790f36a5286f3
Raphael Melo <raphaelgmelo@gmail.com>
2015-10-13 06:57:54 -0300

    music library model

---
+++ Algorhythm/Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by Raphael Melo on 13/10/15.
+//  Copyright © 2015 raphaelgmelo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
